c50c33e6b6a8d02923329c503da477fd94ab6036
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-06-14 19:08:19 +0530

    fixed build

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,31 +1,33 @@
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import AuthProvider from '@/components/AuthProvider';
-import { ToastContainer } from 'react-toastify';
-import { GlobalProvider } from '@/context/GlobalContext';
-import 'react-toastify/dist/ReactToastify.css';
-import 'photoswipe/dist/photoswipe.css';
+import React from 'react'
+import '@/assets/styles/global.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import AuthProvider from '@/components/AuthProvider'
+import { ToastContainer } from 'react-toastify'
+import { GlobalProvider } from '@/context/GlobalContext'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata = {
-  title: 'PropertyPulse | Find The Perfect Rental',
+  title: 'Rentify | Find The Perfect Rental',
   description: 'Find your dream rental property',
-  keywords: 'rental, find rentals, find properties',
-};
+  keywords: 'rental, find rentals, find properties'
+}
 
 const MainLayout = ({ children }) => {
   return (
     <GlobalProvider>
-      <AuthProvider>
-        <html lang='en'>
-          <body>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
-            <ToastContainer />
-          </body>
-        </html>
-      </AuthProvider>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthProvider>
     </GlobalProvider>
-  );
-};
-export default MainLayout;
+  )
+}
+
+export default MainLayout
